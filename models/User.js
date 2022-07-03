429ab3df95e91c3bd320034b61a2fed170ad302e
Jessica <jsisavath@gmail.com>
2022-07-03 17:01:47 -0500

    added routes functionality for user model

--- models/User.js
+++ models/User.js
@@ -1,3 +1,43 @@
-username{ 
-    
-}
+//import only schema constructor, model function, types from mongoose library
+const { Schema, model, Types}= require('mongoose')
+
+const UserSchema = new Schema({
+    username:{
+        type: String, 
+        unique: true, 
+        required: true, 
+        trim:true, 
+    },
+    email:{
+        type: String, 
+        unique:true, 
+        required: true, 
+        match:[/.+@.+\..+/, 'Please enter a valid e-mail address']
+    }, 
+    thoughts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Thought'
+    }],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+}, {
+    toJSON: {
+        virtuals: true,
+        getters: true
+    },
+     // prevents virtuals from creating duplicate of _id as `id`
+    id: false
+});
+
+
+UserSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+})
+
+//creates user model using UserSchema
+const User = model('User', UserSchema);
+
+//export user model 
+module.exports = User;
